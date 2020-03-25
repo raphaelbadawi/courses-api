7ca53f078b4bea86c2d964733d4879604c9b7014
Raphaël Badawi <contact@raphaelbadawi.fr>
2020-03-25 13:07:34 +0100

    Initial Express Routing

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const app = express();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 // Load .env variables
 dotenv.config({
   path: './config/config.env'
 });
 
-const app = express();
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
+// Start server
 const PORT = process.env.PORT || 5000;
 
 app.listen(
